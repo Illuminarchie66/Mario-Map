f42abf1d2a5bfbb807c38f6e7e876937565c0126
Illuminarchie66 <archie.harrodine@outlook.com>
2024-08-16 00:29:43 +0100

    eb

---
+++ application.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import pandas as pd
+
+application = Flask(__name__)
+
+@application.route('/')
+def home():
+    waypoints = pd.read_csv('static/popups/Waypoints.csv')
+    waypoints_json = waypoints.to_json(orient='records')
+    arrows = pd.read_csv('static/popups/Arrows.csv')
+    arrows_json = arrows.to_json(orient='records')
+    print(arrows_json)
+    return render_template('index.html', waypoints=waypoints_json, arrows=arrows_json)
+
+if __name__ == '__main__':
+    application.run(debug=True)
