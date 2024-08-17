148df4aaf606f0ca74c14b089ecfb78b4e103607
Illuminarchie66 <archie.harrodine@outlook.com>
2024-08-17 12:36:31 +0100

    application

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 import pandas as pd
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     waypoints = pd.read_csv('static/popups/Waypoints.csv')
     waypoints_json = waypoints.to_json(orient='records')
@@ -13,4 +13,4 @@ def home():
     return render_template('index.html', waypoints=waypoints_json, arrows=arrows_json)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
