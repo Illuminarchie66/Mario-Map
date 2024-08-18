a44c736d8c0547a0b505293a1cb0634460730c15
Illuminarchie66 <archie.harrodine@outlook.com>
2024-08-18 14:04:09 +0100

    Fullscreen

--- static/scripts/next.js
+++ static/scripts/next.js
@@ -5,6 +5,7 @@ var map = L.map('map', {
     zoomDelta: 0.5,
     zoomSnap: 0.5,
     maxBoundsViscosity: 1.0,
+    fullscreenControl: true
 }).setView([-68.95, 80.35], 6);
 
 L.tileLayer('https://mario-map.s3.eu-west-2.amazonaws.com/Tiles/{z}/{x}/{y}.png', {
@@ -16,16 +17,6 @@ L.tileLayer('https://mario-map.s3.eu-west-2.amazonaws.com/Tiles/{z}/{x}/{y}.png'
 }).addTo(map);
 
 map.setMaxBounds([[-292, -150], [100, 330]])
-
-
-const img = new Image();
-img.src = 'static/popups/images/3D1.png'; // Replace with your image path
-
-img.onload = function() {
-    const width = img.width;
-    const height = img.height;
-    console.log(width);
-};
 
 //Uncomment this code to add an additional marker which will tell you the position it is at.
 // var marker = L.marker([-69.95, 80.35], {
